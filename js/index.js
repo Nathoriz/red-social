fe06bdb52d8742691d7c92f957d260569be48345
kamalyzl <kamaly.kazal@gmail.com>
2018-01-04 20:48:39 -0500

    Primera interaccion

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
 
+
+// var ref = config.databaseURL;
+// console.log(ref);
+// ref.child('webName').on('value',function(snapshot){
+//    console.log(snapshot.val());
+
+// });
+
+
+
 $('#boton').on('click', function () {
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function (result) {
@@ -6,11 +16,11 @@ $('#boton').on('click', function () {
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
+        // saveDate(user);
         console.log(user);
+        $('#showPhoto').append("<img src='"+user.photoURL+"' />")
         // ... 
-    })
-    
-    .catch(function (error) {
+    }).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -23,3 +33,25 @@ $('#boton').on('click', function () {
 
 
 })
+
+
+
+
+
+//funcion para que se guarde la informacion automaticamente 
+// function saveDate(user) {
+//     var usuario = {
+//         nombre:user.displyaName,
+//         email:user.email,
+//         foto:user.photoURL
+//     }
+//     firebase.database().ref('red-social1').push(usuario);
+// }
+
+// $('#save').on('click',function(){
+//     firebase.database().ref('bolsa').set({
+//         nombre:'blis',
+//         edad:15,
+//         sexo:femenino
+//     })
+// })
