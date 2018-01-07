00eb6758ef6ee83bd9dac4b0d3fa72a4ed6b0cfb
kamalyzl <kamaly.kazal@gmail.com>
2018-01-06 20:46:12 -0500

    Agregando filtro en formulario

--- js/index.js
+++ js/index.js
@@ -5,16 +5,16 @@ $(document).ready(function () {
     // Llamando elementos del html 
     $name = $('#first_name');
     $last = $('#last_name');
-    $email = $('#email');
-    $password = $('#password');
+    $email = $('#emailSignUp');
+    $password = $('#passwordSignUp');
     $passConf = $('#passConfir');
     $butonSign = $('#btn-registrar');
     $logOut = $('#btn-logout');
     $btnAauth = $('#btn-firebaseAuth');
 
 
-    $emailModal = $('#emailModal');
-    $passwordModal = $('#passwordModal');
+    $emailLogin = $('#email');
+    $passLogin = $('#pass');
     $btnLogin = $('#btn-login');
 
     //Variables verificadoras booleanas
@@ -82,141 +82,129 @@ $(document).ready(function () {
     }
 
     $btnLogin.on('click', function () {
-        var $email = $emailModal.val();
-        var $password = $passwordModal.val();
+        const email = $emailLogin.val();
+        const password = $passLogin.val();
+        const auth = firebase.auth();
+        const promise = auth.signInWithEmailAndPassword(email, password);
+        promise.catch(function (error) {
+
+            alert("correo no existe");
+        });
+
+    })
+
+    $btnAauth.on('click', function (e) {
+        var emailA = $email.val();
+        var pass = $password.val();
         var auth = firebase.auth();
-        var promise = auth.signInWithEmailAndPassword($email, $password);
-        promise.catch(e => console.log("hola"));
-
+        var promise = auth.createUserWithEmailAndPassword(emailA, pass);
+        promise.catch(function (error) {
+            alert("no fue creado el correo");
+        });
     })
 
-    // $btnAauth.on('click', function () {
-    //     var email = $email.val();
-    //     var password = $password.val();
-    //     var auth = firebase.auth();
-    //     var promise = auth.createUserWithEmailAndPassword(email, password);
-    //     console.log(auth);
-    //     promise.catch(e => console.log(e.message));
-    // })
 
-    $btnAauth.on('click', function () {
-        var referencia = firebase.database().ref('registro');
-        referencia.push({
-            nombre: $name.val(), 
-            contrasena: $passConf.val(),
-            apellido: $last.val()
-        }).then(function () {
-            console.log('dato almacenado correctamente');
-        }).catch(function (error) {
-            console.log('detectado un error', error);
-            // firebase.database().ref('registro').push({
-            //     nombre: $name.val(),
-            //     contrasena: $passConf.val(),
-            //     apellido: $last.val()
-            // })
-        })
-
-        $logOut.on('click', function (e) {
-            firebase.auth().onAuthStateChanged(function (user) {
-                if (user) {
-                    console.log(user);
-                    var displayName = user.displayName;
-                    var email = user.email;
-                    var emailVerified = user.emailVerified;
-                    var photoURL = user.photoURL;
-                    var isAnonymous = user.isAnonymous;
-                    var uid = user.uid;
-                    var providerData = user.providerData;
-                    $logOut.removeClass('hide');
-                } else {
-                    console.log('no logeado');
-                    $logOut.addClass('hide');
-                    // User is signed out.
-                    // ...
-                }
-            });
-
-        })
-
-
-        //Funcion para mostrar imagen
-        function showImage(user) {
-            $('#showPhoto').append("<img src='" + user.photoURL + "' />");
-            $('#namePerson').append("<p>" + user.displayName + "</p>");
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            console.log(user);
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            $logOut.removeClass('hide');
+            console.log('logeado');
+        } else {
+            console.log('no logeado');
+            $logOut.addClass('hide');
+            // User is signed out.
+            // ...
         }
-        // funcion para que se guarde la informacion automaticamente en firebase sin repetirse
-        function saveDate(user) {
-            var usuario = {
-                uid: user.uid,
-                nombre: user.displayName,
-                email: user.email,
-                foto: user.photoURL
-            }
-            firebase.database().ref('redes/' + user.uid).set(usuario);
-        }
+    });
 
-
-        //Agregamos eventos a nuestros input
-        $password.on('keyup', function () {
-            var input = $password.val();
-            // var regex = /^[a-zA-Z]*$/;
-            if (input.length > 3) {
-                verifyPassword = true;
-                activeBoton();
-            } else {
-                desactiveBoton();
-            }
-        });
-
-        // $name.on('keyup', function (e) {
-        //     console.log(e);
-        //     var input = $name.val();
-        //     var regex = /^[a-zA-Z]*$/;
-        //     if (regex.test(input) && input.length > 3) {
-        //         verifyName = true;
-        //         activeBoton();
-        //         $buton.removeClass('disabled');
-        //     } else {
-        //         desactiveBoton();
-        //     }
-        // });
-
-        // $last.on('keyup focus', function () {
-        //     var input = $last.val();
-        //     var regex = /^[a-zA-Z]*$/;
-        //     if (regex.test(input) && input.length > 3) {
-        //         verifyLastName = true;
-        //         activeBoton();
-        //     } else {
-        //         desactiveBoton();
-        //     }
-        // });
-
-        $email.on('keyup focus', function () {
-            var input = $email.val();
-            var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            if (regex.test(input)) {
-                verifyEmail = true;
-                activeBoton();
-            } else {
-                desactiveBoton();
-            }
-
-        });
-
-        function activeBoton() {
-            //verifyName && verifyLastName && verifycheck
-            if (verifyName && verifyLastName && verifyEmail) {
-                $butonSign.removeClass('disabled');
-            }
-        }
-
-        function desactiveBoton() {
-            $butonSign.addClass('disabled');
-        }
-
-
-
-
+    $logOut.on('click', function (e) {
+        firebase.auth().signOut();
     })
+
+    //Funcion para mostrar imagen
+    function showImage(user) {
+        $('#showPhoto').append("<img src='" + user.photoURL + "' />");
+        $('#namePerson').append("<p>" + user.displayName + "</p>");
+    }
+    // funcion para que se guarde la informacion automaticamente en firebase sin repetirse
+    function saveDate(user) {
+        var usuario = {
+            uid: user.uid,
+            nombre: user.displayName,
+            email: user.email,
+            foto: user.photoURL
+        }
+        firebase.database().ref('redes/' + user.uid).set(usuario);
+    }
+
+
+    //Agregamos eventos a nuestros input
+    // $password.on('keyup', function () {
+    //     var input = $password.val();
+    //     // var regex = /^[a-zA-Z]*$/;
+    //     if (input.length > 3) {
+    //         verifyPassword = true;
+    //         activeBoton();
+    //     } else {
+    //         desactiveBoton();
+    //     }
+    // });
+
+    // $name.on('keyup', function (e) {
+    //     console.log(e);
+    //     var input = $name.val();
+    //     var regex = /^[a-zA-Z]*$/;
+    //     if (regex.test(input) && input.length > 3) {
+    //         verifyName = true;
+    //         activeBoton();
+    //         $buton.removeClass('disabled');
+    //     } else {
+    //         desactiveBoton();
+    //     }
+    // });
+
+    // $last.on('keyup focus', function () {
+    //     var input = $last.val();
+    //     var regex = /^[a-zA-Z]*$/;
+    //     if (regex.test(input) && input.length > 3) {
+    //         verifyLastName = true;
+    //         activeBoton();
+    //     } else {
+    //         desactiveBoton();
+    //     }
+    // });
+
+    // $email.on('keyup focus', function () {
+    //     var input = $email.val();
+    //     var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    //     if (regex.test(input)) {
+    //         verifyEmail = true;
+    //         activeBoton();
+    //     } else {
+    //         desactiveBoton();
+    //     }
+
+    // });
+
+    function activeBoton() {
+        //verifyName && verifyLastName && verifycheck
+        if (verifyName && verifyLastName && verifyEmail) {
+            $butonSign.removeClass('disabled');
+        }
+    }
+
+    function desactiveBoton() {
+        $butonSign.addClass('disabled');
+    }
+
+
+
+
 })
