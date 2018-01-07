b5c22357818f8955fdbc53445eacad36be5d82ca
kamalyzl <kamaly.kazal@gmail.com>
2018-01-06 20:29:41 -0500

    Añadiendo ed-grid y nuevo registro independiente

--- js/index.js
+++ js/index.js
@@ -1,37 +1,34 @@
 
 $(document).ready(function () {
     //Nav mobile Materialize
-    $(".button-collapse").sideNav();
+    // $(".button-collapse").sideNav();
 
     //Materialize fecha de nacimiento para formulario
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 90, // Creates a dropdown of 15 years to control year,
-        today: 'Today',
-        clear: 'Clear',
-        close: 'Ok',
-        closeOnSelect: false // Close upon selecting a date,
-      });
-        
-      
+    // $('.datepicker').pickadate({
+    //     selectMonths: true, // Creates a dropdown to control month
+    //     selectYears: 90, // Creates a dropdown of 15 years to control year,
+    //     today: 'Today',
+    //     clear: 'Clear',
+    //     close: 'Ok',
+    //     closeOnSelect: false // Close upon selecting a date,
+    //   });
+
+
 
 
 
     //Agregando funcionalidad al boton de facebook 
-    $('#botonFacebook').on('click', function () {
+
+    $('#btn-facebook').on('click', loginFacebook);
+    function loginFacebook() {
         if (!firebase.auth().currentUser) {
             var provider = new firebase.auth.FacebookAuthProvider();
-            provider.addScope('public_profile');
+            // provider.addScope('https:www.googleapis.com/auth/plus.login');
             firebase.auth().signInWithPopup(provider).then(function (result) {
                 var token = result.credential.accessToken;
                 var user = result.user;
-                console.log(user);
                 saveDate(user);
-                $('#botonFacebook').fadeOut();
-                $(".hide").removeClass();
-
-                $('#showPhoto').append("<img src='" + user.photoURL + "' />");
-                $('#namePerson').append("<p>" + user.displayName + "</p>");
+                showImage(user)
             }).catch(function (error) {
                 // Handle Errors here.
                 var errorCode = error.code;
@@ -42,33 +39,24 @@ $(document).ready(function () {
                 var credential = error.credential;
                 if (error.code === 'auth/account-exists-with-different-credential') {
                     alert('Es el mismo usuario');
-
                 }
             });
         }
         else {
             firebase.auth().signOut();
         }
-    })
+    }
 
-
-
-
-    //Agregando funcionalidad al boton de google 
-    $('#botonGoogle').on('click', function () {
+    $('#btn-google').on('click', loginGoogle);
+    function loginGoogle() {
         if (!firebase.auth().currentUser) {
             var provider = new firebase.auth.GoogleAuthProvider();
             // provider.addScope('https:www.googleapis.com/auth/plus.login');
             firebase.auth().signInWithPopup(provider).then(function (result) {
                 var token = result.credential.accessToken;
                 var user = result.user;
-                // console.log(user);
-                // saveDate(user);
-                $('#boton').fadeOut();
-                $(".hide").removeClass();
-
-                $('#showPhoto').append("<img src='" + user.photoURL + "' />");
-                $('#namePerson').append("<p>" + user.displayName + "</p>");
+                saveDate(user);
+                showImage(user)
             }).catch(function (error) {
                 // Handle Errors here.
                 var errorCode = error.code;
@@ -79,15 +67,19 @@ $(document).ready(function () {
                 var credential = error.credential;
                 if (error.code === 'auth/account-exists-with-different-credential') {
                     alert('Es el mismo usuario');
-
                 }
             });
         }
         else {
             firebase.auth().signOut();
         }
-    })
+    }
 
+    //Funcion para mostrar imagen
+    function showImage(user) {
+        $('#showPhoto').append("<img src='" + user.photoURL + "' />");
+        $('#namePerson').append("<p>" + user.displayName + "</p>");
+    }
     // funcion para que se guarde la informacion automaticamente en firebase sin repetirse
     function saveDate(user) {
         var usuario = {
@@ -96,24 +88,121 @@ $(document).ready(function () {
             email: user.email,
             foto: user.photoURL
         }
-        firebase.database().ref('object/' + user.uid).set(usuario);
+        firebase.database().ref('redes/' + user.uid).set(usuario);
     }
 
+
+
     //boton donde se puede escribir en la base de datos y enviarse a firebase
-    $('#save').on('click', function () {
-        firebase.database().ref('prueba').set({
-            edad: 15,
-            name: 'y ahora que paso',
-            sexo: 'quien sabe'
+    $('#btn-registrar').on('click', function () {
+        firebase.database().ref('registro').set({
+            nombre: $name.val(),
+            apellido: $last.val(),
+            email: $email.val(),
+            clave: $password.val(),
+            acepta: $checkbox.val()
         })
     })
 
-    // firebase.database().ref('object').on('child_added', function (s) {
-    //     var user = s.value;
-    //     $('#showPhoto').append("<img src='" + user.foto+ "' />")
-    // })
+
+// AQUI HACEMOS LA VALIDACION DE NUESTRO BOTON INDEPENDIENTE REGISTRO 
+    // Llamando elementos del html 
+    $name = $('#first_name');
+    $last = $('#last_name');
+    $email = $('#email');
+    $checkbox = $("#test5");
+    $buton = $('#btn-registrar');
+    $password = $('#password');
 
 
+    //Variables verificadoras booleanas
+    var verifyName = false;
+    var verifyLastName = false;
+    var verifyEmail = false;
+    var verifycheck = false;
+    var verifyPassword = false;
+
+    //Agregamos eventos a nuestros input
+
+    $password.on('keyup focus', function () {
+        var input = $password.val();
+        // var regex = /^[a-zA-Z]*$/;
+        if (input.length > 3) {
+            verifyPassword = true;
+            activeBoton()
+        } else {
+            desactiveBoton();
+        }
+
+
+    });
+
+
+    $name.on('keyup focus', function () {
+        var input = $name.val();
+        var regex = /^[a-zA-Z]*$/;
+        if (regex.test(input) && input.length > 3) {
+            verifyName = true;
+            activeBoton()
+        } else {
+            desactiveBoton();
+        }
+
+
+    });
+
+    $last.on('keyup focus', function () {
+        var input = $last.val();
+        var regex = /^[a-zA-Z]*$/;
+        if (regex.test(input) && input.length > 3) {
+            verifyLastName = true;
+            activeBoton();
+        } else {
+            desactiveBoton();
+        }
+
+
+    });
+
+    $email.on('keyup focus', function () {
+        var input = $email.val();
+        var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (regex.test(input)) {
+            verifyEmail = true;
+            activeBoton();
+        } else {
+            desactiveBoton();
+        }
+
+    });
+    $checkbox.on('click', function () {
+        if ($checkbox.prop("checked")) {
+            verifycheck = true;
+            activeBoton();
+        } else {
+            desactiveBoton();
+        }
+
+    });
+    $buton.on('click', function () {
+        $name.val("")
+        $last.val("");
+        $email.val("")
+        $checkbox.val("");
+        $buton.val("");
+        // window.location.href = "final.html";
+        alert('datos enviados')
+    });
+
+    function activeBoton() {
+        if (verifyName && verifyLastName && verifyEmail && verifycheck && verifyPassword) {
+            $buton.removeClass('disabled');
+        }
+    }
+
+    function desactiveBoton() {
+        $buton.addClass('disabled');
+    }
 
 
 
