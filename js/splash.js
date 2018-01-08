31f93b2162302428ba3a224face1ae424210ab56
Nathoriz <nathortiz_41@hotmail.com>
2018-01-08 12:38:52 -0500

    Añadiendo vista splash

---
+++ js/splash.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    // Animacion
+    var svgAttributes = anime({
+        targets: '#svgAttributes polygon',
+        points: '64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96',
+        easing: 'easeInOutExpo'
+      });
+});
