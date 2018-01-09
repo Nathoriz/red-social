1fb0da4bebd7d8cb547d09c4593b03002dc9b60c
YasminMattos <yasmin.mattos.conislla@gmail.com>
2018-01-09 10:12:17 -0500

    search and buttons

--- js/home.js
+++ js/home.js
@@ -1,21 +1,32 @@
-iPerfil = $('#perfil');
-iHome = $('#home');
-iSearch = $('#search');
-iNoti = $('#notifi');
-
-iPerfil.on('click',function(){
+$(document).ready(function(){
+  iPerfil = $('#perfil');
+  iHome = $('#home');
+  iSearch = $('#search');
+  iNoti = $('#notifi');
+  search = $('.search-box')
+  
+  iPerfil.on('click',function(){
     $('.ide2').addClass('hide');
+    search.addClass('hide');
     $('.ide1').removeClass('hide');
-
-})
-iHome.on('click',function(){
+  
+  })
+  iHome.on('click',function(){
     $('.ide1').addClass('hide');
-    $('.ide2').removeClass('hide');
-
-})
-
-iNoti.on('click',function(){
     $('.ide2').addClass('hide');
-    $('.ide1').removeClass('hide');
+    search.addClass('hide');
+  
+  })
+  
+  iSearch.on('click', function(){
+    $('.ide2').addClass('hide');
+    search.removeClass('hide');
+  })
 
+  iNoti.on('click',function(){
+    $('.ide2').removeClass('hide');
+    $('.ide1').addClass('hide');
+    search.addClass('hide');
+  
+  })
 })
