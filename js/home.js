038fcba59ce637c2e99aefac91a8a76e8e510c2b
YasminMattos <yasmin.mattos.conislla@gmail.com>
2018-01-09 19:24:10 -0500

    mejora estructura

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  iPerfil = $('#perfil');
+  iProfile = $('#profile');
   iHome = $('#home');
   iSearch = $('#search');
   iNoti = $('#notifi');
@@ -7,35 +7,35 @@ $(document).ready(function() {
 
   $('.modal').modal();
   
-  iPerfil.on('click',function() {
+  iProfile.on('click', function() {
+    $('.ide1').removeClass('hide');
     $('.ide2').addClass('hide');
     search.addClass('hide');
-    $('.ide1').removeClass('hide');
-  
-  })
-  iHome.on('click',function() {
+    $('.ide4').addClass('hide');
+  });
+
+  iHome.on('click', function() {
     $('.ide1').addClass('hide');
     $('.ide2').addClass('hide');
     search.addClass('hide');
-    $('.ide3').removeClass('hide');
-  
-  })
+    $('.ide4').removeClass('hide');
+  });
   
   iSearch.on('click', function() {
     $('.ide1').addClass('hide');
     $('.ide2').addClass('hide');
+    $('.ide4').addClass('hide');
     search.removeClass('hide');
-  })
+  });
 
-  iNoti.on('click',function() {
+  iNoti.on('click', function() {
     $('.ide2').removeClass('hide');
     $('.ide1').addClass('hide');
     search.addClass('hide');
-  })
-| 
+    $('.ide4').addClass('hide');
+  });
+
   $('.friend').on('click', function() {
     alert('dsjgf');
-  })
-
-
-})
+  });
+});
